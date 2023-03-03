bf299dde4dd6be8a371b9b8a068a3ac8be65a924
yogigan <bachtiarnuryogipratama1@gmail.com>
2023-03-03 16:49:38 +0700

    init gui

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,37 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javax.swing.*;
+
+public class App extends JFrame {
+    private JPanel jPanel;
+    private JTextField txtInputBaseUrl;
+    private JTextField txtInputPath;
+    private JTextArea txtAreaBodyRequest;
+    private JTextArea txtAreaResponse;
+    private JTextField txtInputUser;
+    private JTextField txtInputPassword;
+    private JTextField txtInputInKey;
+    private JTextField txtInputHeader;
+    private JTextArea txtAreaHmac;
+    private JButton btnGenHmac;
+    private JButton btnSend;
+    private JButton btnReset;
+    private JList listAction;
+
+    public App() {
+        setContentPane(jPanel);
+        setTitle("UI Simulator Rest Api");
+        setSize(720, 480);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        App app = new App();
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
     }
 }
